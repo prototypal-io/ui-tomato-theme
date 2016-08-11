7a9e4e3340106213ae2714fd4cef92f30ced28da
Jaco Joubert <joubert.jaco@gmail.com>
2016-08-11 11:33:59 -0400

    add new default input. styling tweaks.

--- app/instance-initializers/register-tomato-theme.js
+++ app/instance-initializers/register-tomato-theme.js
@@ -2,6 +2,7 @@ export function initialize(appInstance) {
   let themeService = appInstance.lookup('service:ui-theme');
 
   themeService.register('ui-button', 'default', 'tomato');
+  themeService.register('ui-input', 'default', 'tomato');
   //                    ^^ base     ^^ kind    ^^ theme
 }
 
