33d998c7851d0c3d0e0f9796be6022d2850d76fe
Eric Kelly <heroiceric@gmail.com>
2016-08-16 12:55:08 -0700

    Add more tomato themed components

--- app/instance-initializers/register-tomato-theme.js
+++ app/instance-initializers/register-tomato-theme.js
@@ -2,10 +2,17 @@ export function initialize(appInstance) {
   let themeService = appInstance.lookup('service:ui-theme');
 
   themeService.register('ui-button', 'default', 'tomato');
+  themeService.register('ui-button', 'primary', 'tomato');
+  themeService.register('ui-button', 'simple', 'tomato');
+
   themeService.register('ui-input', 'default', 'tomato');
   themeService.register('ui-field', 'default', 'tomato');
   themeService.register('ui-tooltip', 'default', 'tomato');
   //                    ^^ base     ^^ kind    ^^ theme
+
+  themeService.register('ui-panel', 'default', 'tomato');
+  themeService.register('ui-panel---content', 'default', 'tomato');
+  themeService.register('ui-panel---titlebar', 'default', 'tomato');
 }
 
 export default {
